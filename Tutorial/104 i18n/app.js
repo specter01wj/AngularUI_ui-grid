4b96c2b340f9a89095823fbe064d1e5fa6f83263
specter01wj <specter01wj@gmail.com>
2017-09-09 19:19:45 -0400

    09/09/2017 v1

---
+++ Tutorial/104 i18n/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('app', ['ngTouch', 'ui.grid']);
+
+app.controller('MainCtrl', ['$scope', 'i18nService', '$http', function ($scope, i18nService, $http) {
+  $scope.langs = i18nService.getAllLangs();
+  $scope.lang = 'nl';
+
+  $scope.gridOptions = {
+    columnDefs: [
+      { field: 'name' },
+      { field: 'gender' },
+      { field: 'company', enableFiltering: false  }
+    ]
+  };
+
+  $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/100.json')
+  .success(function(data) {
+    $scope.gridOptions.data = data;
+  });
+}]);
